8e1a5957d075ff7459e606886e8e715e68a9985f
Skarga Dmitry <55750642+13090903@users.noreply.github.com>
2024-03-27 17:09:24 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,210 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RationalNumberApp.Model;
+
+namespace RationalNumberApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            if (numerator1.Text == "" || denominator1.Text == "" || numerator2.Text == "" || denominator2.Text == "")
+            {
+                MessageBox.Show("Введите все необходимые данные!");
+            }
+            else
+            {
+                long numeratorFirst = Convert.ToInt64(numerator1.Text);
+                long denominatorFirst = Convert.ToInt64(denominator1.Text);
+                long numeratorSecond = Convert.ToInt64(numerator2.Text);
+                long denominatorSecond = Convert.ToInt64(denominator2.Text);
+                if (denominatorFirst == 0 || denominatorSecond == 0)
+                {
+                    result.Text = "Бесконечность";
+                }
+                else
+                {
+                    RationalNumber r1 = new(numeratorFirst, denominatorFirst);
+                    RationalNumber r2 = new(numeratorSecond, denominatorSecond);
+                    RationalNumber res = r1 + r2;
+                    result.Text = res.ToString();
+                }
+            }
+        }
+        private void Subtract(object sender, RoutedEventArgs e)
+        {
+            if (numerator1.Text == "" || denominator1.Text == "" || numerator2.Text == "" || denominator2.Text == "")
+            {
+                MessageBox.Show("Введите все необходимые данные!");
+            }
+            else
+            {
+                long numeratorFirst = Convert.ToInt64(numerator1.Text);
+                long denominatorFirst = Convert.ToInt64(denominator1.Text);
+                long numeratorSecond = Convert.ToInt64(numerator2.Text);
+                long denominatorSecond = Convert.ToInt64(denominator2.Text);
+                if (denominatorFirst == 0 || denominatorSecond == 0)
+                {
+                    result.Text = "Бесконечность";
+                }
+                else
+                {
+                    RationalNumber r1 = new(numeratorFirst, denominatorFirst);
+                    RationalNumber r2 = new(numeratorSecond, denominatorSecond);
+                    RationalNumber res = r1 - r2;
+                    result.Text = res.ToString();
+                }
+            }
+        }
+        private void Multiply(object sender, RoutedEventArgs e)
+        {
+            if (numerator1.Text == "" || denominator1.Text == "" || numerator2.Text == "" || denominator2.Text == "")
+            {
+                MessageBox.Show("Введите все необходимые данные!");
+            }
+            else
+            {
+                long numeratorFirst = Convert.ToInt64(numerator1.Text);
+                long denominatorFirst = Convert.ToInt64(denominator1.Text);
+                long numeratorSecond = Convert.ToInt64(numerator2.Text);
+                long denominatorSecond = Convert.ToInt64(denominator2.Text);
+                if (denominatorFirst == 0 || denominatorSecond == 0)
+                {
+                    result.Text = "Бесконечность";
+                }
+                else
+                {
+                    RationalNumber r1 = new(numeratorFirst, denominatorFirst);
+                    RationalNumber r2 = new(numeratorSecond, denominatorSecond);
+                    RationalNumber res = r1 * r2;
+                    result.Text = res.ToString();
+                }
+            }
+        }
+        private void Divide(object sender, RoutedEventArgs e)
+        {
+            if (numerator1.Text == "" || denominator1.Text == "" || numerator2.Text == "" || denominator2.Text == "")
+            {
+                MessageBox.Show("Введите все необходимые данные!");
+            }
+            else
+            {
+                long numeratorFirst = Convert.ToInt64(numerator1.Text);
+                long denominatorFirst = Convert.ToInt64(denominator1.Text);
+                long numeratorSecond = Convert.ToInt64(numerator2.Text);
+                long denominatorSecond = Convert.ToInt64(denominator2.Text);
+                if (denominatorFirst == 0 || denominatorSecond == 0)
+                {
+                    result.Text = "Бесконечность";
+                }
+                else
+                {
+                    RationalNumber r1 = new(numeratorFirst, denominatorFirst);
+                    RationalNumber r2 = new(numeratorSecond, denominatorSecond);
+                    RationalNumber res = r1 / r2;
+                    result.Text = res.ToString();
+                }
+            }
+        }
+
+        private void Normalize(object sender, RoutedEventArgs e)
+        {
+            if (numerator1.Text == "" || denominator1.Text == "")
+            {
+                MessageBox.Show("Введите все необходимые данные!");
+            }
+            else
+            {
+                long numeratorFirst = Convert.ToInt64(numerator1.Text);
+                long denominatorFirst = Convert.ToInt64(denominator1.Text);
+                if (denominatorFirst == 0)
+                {
+                    result.Text = "Бесконечность";
+                }
+                else
+                {
+                    RationalNumber r1 = new(numeratorFirst, denominatorFirst);
+                    result.Text = r1.ToString();
+                }
+            }
+        }
+
+        private void Compare(object sender, RoutedEventArgs e)
+        {
+            if (numerator1.Text == "" || denominator1.Text == "" || numerator2.Text == "" || denominator2.Text == "")
+            {
+                MessageBox.Show("Введите все необходимые данные!");
+            }
+            else
+            {
+                long numeratorFirst = Convert.ToInt64(numerator1.Text);
+                long denominatorFirst = Convert.ToInt64(denominator1.Text);
+                long numeratorSecond = Convert.ToInt64(numerator2.Text);
+                long denominatorSecond = Convert.ToInt64(denominator2.Text);
+                if (denominatorFirst == 0 || denominatorSecond == 0)
+                {
+                    result.Text = "Неопределенность";
+                }
+                else
+                {
+                    RationalNumber r1 = new(numeratorFirst, denominatorFirst);
+                    RationalNumber r2 = new(numeratorSecond, denominatorSecond);
+                    result.Text = RationalNumber.Compare(r1, r2);
+                }
+            }
+        }
+
+        private void Convertation(object sender, RoutedEventArgs e)
+        {
+            if (numerator1.Text == "" || denominator1.Text == "" || amount.Text == "")
+            {
+                MessageBox.Show("Введите все необходимые данные!");
+            }
+            else
+            {
+                long numeratorFirst = Convert.ToInt64(numerator1.Text);
+                long denominatorFirst = Convert.ToInt64(denominator1.Text);
+                RationalNumber r1 = new(numeratorFirst, denominatorFirst);
+                double res = RationalNumber.AsDouble(r1, Convert.ToInt32(amount.Text));
+                result.Text = res.ToString();
+            }
+        }
+
+        private void ConvertationRev(object sender, RoutedEventArgs e)
+        {
+            if (doubleNum.Text == "" || amount1.Text == "")
+            {
+                MessageBox.Show("Введите все необходимые данные!");
+            }
+            else
+            {
+                double number = Convert.ToDouble(doubleNum.Text);
+                RationalNumber res = RationalNumber.AsDecimal(number, Convert.ToInt32(amount1.Text));
+                result.Text = res.ToString();
+            }
+        }
+    }
+}
---
+++ Model/RationalNumber.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace RationalNumberApp.Model
+{
+    public class RationalNumber
+    {
+        private long _numerator;
+        private long _denominator;
+        public RationalNumber(long numerator, long denominator)
+        {
+            _numerator = numerator;
+            _denominator = denominator;
+            Norm();
+        }
+
+        public void Norm()
+        {
+            int sign1 = _numerator < 0 ? -1 : 1;
+            int sign2 = _denominator < 0 ? -1 : 1;
+            int sign = sign1 * sign2;
+            long g = Gcd(_numerator, _denominator);
+            _numerator = sign * Math.Abs(_numerator / g);
+            _denominator = Math.Abs(_denominator / g);
+        }
+
+        private static long Gcd(long a, long b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            return Gcd(b, a % b);
+        }
+
+        override
+        public string ToString()
+        {
+            return _numerator + " / " + _denominator;
+        }
+
+        public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
+        {
+            RationalNumber res = new RationalNumber(r1._numerator * r2._denominator + r2._numerator * r1._denominator,
+                r1._denominator * r2._denominator);
+            return res;
+        }
+
+        public static RationalNumber operator -(RationalNumber r1, RationalNumber r2)
+        {
+            RationalNumber res = new RationalNumber(r1._numerator * r2._denominator - r2._numerator * r1._denominator,
+                r1._denominator * r2._denominator);
+            return res;
+        }
+
+        public static RationalNumber operator *(RationalNumber r1, RationalNumber r2)
+        {
+            RationalNumber res = new(r1._numerator * r2._numerator,
+                r1._denominator * r2._denominator);
+            return res;
+        }
+        public static RationalNumber operator /(RationalNumber r1, RationalNumber r2)
+        {
+            RationalNumber res = new(r1._numerator * r2._denominator,
+                r1._denominator * r2._numerator);
+            return res;
+        }
+
+        public static string Compare(RationalNumber r1, RationalNumber r2)
+        {
+            RationalNumber res = r1 - r2;
+            int sign1 = res._numerator < 0 ? -1 : 1;
+            int sign2 = res._denominator < 0 ? -1 : 1;
+            int sign = sign1 * sign2;
+            if (res._numerator == 0) {
+                return "Числа равны";
+            }
+            if (sign < 0) {
+                return "Второе число больше";
+            }
+            return "Первое число больше";
+        }
+
+        public static double AsDouble(RationalNumber r1, int decPl)
+        {
+            double res = 1.0*r1._numerator / r1._denominator;
+            res = Math.Round(res * Math.Pow(10, decPl))/ Math.Pow(10, decPl);
+            return res;
+        }
+
+        public static RationalNumber AsDecimal(double num, int decPl)
+        {
+            double res = num;
+            res = Math.Round(res * Math.Pow(10, decPl)) / Math.Pow(10, decPl);
+            RationalNumber result = new((long)(res * Math.Pow(10, decPl)), (long)Math.Pow(10, decPl));
+            return result;
+        }
+    }
+}
